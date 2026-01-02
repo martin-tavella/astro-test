979637ed9e01dc80577c0a76bf7ada3268fc7f56
martin-tavella <martutavella@gmail.com>
2026-01-02 20:41:38 -0300

    feat: testing Server Island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  env: {
+    schema: {
+      SCORE_API_ENDPOINT: envField.string({context: "server", access: "public"})
+    }
+  },
+
+  adapter: netlify()
 });
